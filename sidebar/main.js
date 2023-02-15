7df53c76def26763e41ac32964cb6a468b15716e
unknown <sergio.gomez2000@hotmail.com>
2023-02-14 19:05:51 -0500

    [ADD] funcionality and styles

--- sidebar/main.js
+++ sidebar/main.js
@@ -1,4 +1,28 @@
-const selected = document.querySelector('.selected');
+const sidebar_container = document.getElementById('sidebar-container')
+const item_home = document.getElementById('item-home');
+const item_search = document.getElementById('item-search');
+const item_user = document.getElementById('item-user');
+const item_extras = document.getElementById('item-extras');
+const toggle = document.querySelector('.toggle');
+var selected = item_home;
 
 
-selected.style.color = "gray";
+const clicked = (item) => {
+    selected.classList.remove('selected');
+    item.classList.add('selected');
+    selected = item;
+};
+
+item_home.onclick = () => clicked(item_home);
+item_search.onclick = () => clicked(item_search);
+item_user.onclick = () => clicked(item_user);
+item_extras.onclick = () => clicked(item_extras);
+toggle.onclick = () => {
+    sidebar_container.classList.toggle('shrink');
+    if (sidebar_container.classList.contains('shrink')){
+    toggle.innerHTML = '<img  src="Assets/Burger Menu.svg"></img>'
+    } else {
+        toggle.innerHTML = '<img  src="Assets/Back arrow.svg">'
+
+    }
+}
